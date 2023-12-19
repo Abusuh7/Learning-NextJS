7c7fe210d76754595e96b8d086d2caa9df2c1786
Abdullah Suhail <107505201+Abusuh7@users.noreply.github.com>
2023-12-19 18:32:00 +0530

    study

---
+++ app/products/[productid]/page.js
@@ -0,0 +1,5 @@
+export default function Productid({params}) {
+    return (
+        <h1>Details about product {params.productid}</h1>
+    )
+}
---
+++ app/products/page.js
@@ -0,0 +1,5 @@
+export default function Productid() {
+    return (
+        <h1>Details about product</h1>
+    )
+}
