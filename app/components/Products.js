0028bf59267db4b970731d13152dceac08c0754f
Abdullah Suhail <107505201+Abusuh7@users.noreply.github.com>
2023-12-22 17:41:40 +0530

    update

--- app/components/Products.js
+++ app/components/Products.js
@@ -1,6 +1,6 @@
 export default function Products({title, price}) {
     return (
-        <div>
+        <div style={{border: "1px solid black", padding: "20px", margin: "20px"}}>
             <h2>{title}</h2>
             <p>{price}</p>
         </div>
