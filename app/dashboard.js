53cb2644e67e2f14c62bb5aea5bd14896a05b364
Abdullah Suhail <107505201+Abusuh7@users.noreply.github.com>
2024-01-08 09:31:46 +0530

    update

---
+++ app/dashboard.js
@@ -0,0 +1,10 @@
+export default class Dashboard extends React.Component {
+    render() {
+        return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>This is the dashboard</p>
+        </div>
+        );
+    }
+    }
